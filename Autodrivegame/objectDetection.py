6dc150a64432415ec274189886b74e40d1acfd15
yunminjin2 <56184924+yunminjin2@users.noreply.github.com>
2021-08-04 13:23:49 +0900

    object detection added

---
+++ Autodrivegame/objectDetection.py
@@ -0,0 +1,213 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))) + "\myYolact")
+
+from data import COCODetection, get_label_map, MEANS, COLORS
+from yolact import Yolact
+from utils.augmentations import BaseTransform, FastBaseTransform, Resize
+from utils.functions import MovingAverage, ProgressBar
+from layers.box_utils import jaccard, center_size, mask_iou
+from utils import timer
+from utils.functions import SavePath
+from layers.output_utils import postprocess, undo_image_transformation
+import pycocotools
+
+from data import cfg, set_cfg, set_dataset
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+from torch.autograd import Variable
+import argparse
+import time
+import random
+import cProfile
+import pickle
+import json
+#import os
+from collections import defaultdict
+from pathlib import Path
+from collections import OrderedDict
+from PIL import Image
+
+import matplotlib.pyplot as plt
+import cv2
+
+iou_thresholds = [x / 100 for x in range(50, 100, 5)]
+coco_cats = {} # Call prep_coco_cats to fill this
+coco_cats_inv = {}
+color_cache = defaultdict(lambda: {})
+
+
+
+## MJ 08/03 ##
+class objectDetector():
+    def __init__(self, trained_model, 
+                cuda=True, display_lincomb=False, score_threshold=0, crop=False, display_text=True, display_bboxes=True,
+                mask_proto_debug=False, fast_nms=True, cross_class_nms=False,
+                top_k=0, display_masks=True, display_scores=True):
+        self.display_lincomb = display_lincomb
+        self.score_threshold = score_threshold
+        self.crop = crop
+        self.display_text = display_text
+        self.display_bboxes = display_bboxes
+        self.display_scores = display_scores
+        self.display_masks = display_masks
+        self.mask_proto_debug = mask_proto_debug
+        self.fast_nms = fast_nms
+        self.cross_class_nms = cross_class_nms
+
+        self.top_k = top_k
+
+        self.model_path = SavePath.from_str(trained_model)
+        self.config = self.model_path.model_name + '_config'
+        set_cfg(self.config)
+
+        with torch.no_grad():
+            if cuda:
+                cudnn.fastest = True
+                torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+            print('Loading model...', end='')
+            self.net = Yolact()
+            self.net.load_weights(trained_model)
+            self.net.eval()
+            print(' Done.')
+
+            self.net = self.net.cuda()
+
+            self.net.detect.use_fast_nms = self.fast_nms
+            self.net.detect.use_cross_class_nms = self.cross_class_nms
+            
+            cfg.mask_proto_debug = self.mask_proto_debug
+
+    def prep_display(self, dets_out, img, h, w, undo_transform=True, class_color=False, mask_alpha=0.45, fps_str=''):
+        """
+        Note: If undo_transform=False then im_h and im_w are allowed to be None.
+        """
+        # Quick and dirty lambda for selecting the color for a particular index
+        # Also keeps track of a per-gpu color cache for maximum speed
+        def get_color(j, on_gpu=None):
+            global color_cache
+            color_idx = (classes[j] * 5 if class_color else j * 5) % len(COLORS)
+            
+            if on_gpu is not None and color_idx in color_cache[on_gpu]:
+                return color_cache[on_gpu][color_idx]
+            else:
+                color = COLORS[color_idx]
+                if not undo_transform:
+                    # The image might come in as RGB or BRG, depending
+                    color = (color[2], color[1], color[0])
+                if on_gpu is not None:
+                    color = torch.Tensor(color).to(on_gpu).float() / 255.
+                    color_cache[on_gpu][color_idx] = color
+                return color
+
+                
+        if undo_transform:
+            img_numpy = undo_image_transformation(img, w, h)
+            img_gpu = torch.Tensor(img_numpy).cuda()
+        else:
+            img_gpu = img / 255.0
+            h, w, _ = img.shape
+        
+        with timer.env('Postprocess'):
+            save = cfg.rescore_bbox
+            cfg.rescore_bbox = True
+            t = postprocess(dets_out, w, h, visualize_lincomb = self.display_lincomb,
+                                            crop_masks        = self.crop,
+                                            score_threshold   = self.score_threshold)
+            cfg.rescore_bbox = save
+
+        with timer.env('Copy'):
+            idx = t[1].argsort(0, descending=True)[:self.top_k]
+            
+            if cfg.eval_mask_branch:
+                # Masks are drawn on the GPU, so don't copy
+                masks = t[3][idx]
+            classes, scores, boxes = [x[idx].cpu().numpy() for x in t[:3]]
+
+        num_dets_to_consider = min(self.top_k, classes.shape[0])
+        for j in range(num_dets_to_consider):
+            if scores[j] < self.score_threshold:
+                num_dets_to_consider = j
+                break
+
+
+        # First, draw the masks on the GPU where we can do it really fast
+        # Beware: very fast but possibly unintelligible mask-drawing code ahead
+        # I wish I had access to OpenGL or Vulkan but alas, I guess Pytorch tensor operations will have to suffice
+        if self.display_masks and cfg.eval_mask_branch and num_dets_to_consider > 0:
+            # After this, mask is of size [num_dets, h, w, 1]
+            masks = masks[:num_dets_to_consider, :, :, None]
+            
+            # Prepare the RGB images for each mask given their color (size [num_dets, h, w, 1])
+            colors = torch.cat([get_color(j, on_gpu=img_gpu.device.index).view(1, 1, 1, 3) for j in range(num_dets_to_consider)], dim=0)
+            masks_color = masks.repeat(1, 1, 1, 3) * colors * mask_alpha
+
+            # This is 1 everywhere except for 1-mask_alpha where the mask is
+            inv_alph_masks = masks * (-mask_alpha) + 1
+            
+            # I did the math for this on pen and paper. This whole block should be equivalent to:
+            #    for j in range(num_dets_to_consider):
+            #        img_gpu = img_gpu * inv_alph_masks[j] + masks_color[j]
+            masks_color_summand = masks_color[0]
+            if num_dets_to_consider > 1:
+                inv_alph_cumul = inv_alph_masks[:(num_dets_to_consider-1)].cumprod(dim=0)
+                masks_color_cumul = masks_color[1:] * inv_alph_cumul
+                masks_color_summand += masks_color_cumul.sum(dim=0)
+
+            img_gpu = img_gpu * inv_alph_masks.prod(dim=0) + masks_color_summand
+        
+        # Then draw the stuff that needs to be done on the cpu
+        # Note, make sure this is a uint8 tensor or opencv will not anti alias text for whatever reason
+        img_numpy = (img_gpu * 255).byte().cpu().numpy()
+
+        if num_dets_to_consider == 0:
+            return img_numpy
+
+        if self.display_text or self.display_bboxes:
+            for j in reversed(range(num_dets_to_consider)):
+                x1, y1, x2, y2 = boxes[j, :]
+                color = get_color(j)
+                score = scores[j]
+
+                if self.display_bboxes:
+                    cv2.rectangle(img_numpy, (x1, y1), (x2, y2), color, 1)
+
+                if self.display_text:
+                    _class = cfg.dataset.class_names[classes[j]]
+                    text_str = '%s: %.2f' % (_class, score) if self.display_scores else _class
+
+                    font_face = cv2.FONT_HERSHEY_DUPLEX
+                    font_scale = 0.6
+                    font_thickness = 1
+
+                    text_w, text_h = cv2.getTextSize(text_str, font_face, font_scale, font_thickness)[0]
+
+                    text_pt = (x1, y1 - 10)
+                    text_color = [255, 255, 255]
+
+                    cv2.rectangle(img_numpy, (x1, y1), (x1 + text_w, y1 - text_h - 4), color, -1)
+                    cv2.putText(img_numpy, text_str, text_pt, font_face, font_scale, text_color, font_thickness, cv2.LINE_AA)
+                
+        
+        return img_numpy
+
+
+    def evalimage(self, image):
+        frame = torch.from_numpy(image).cuda().float()
+        batch = FastBaseTransform()(frame.unsqueeze(0))
+        preds = self.net(batch)
+
+        img_numpy = self.prep_display(preds, frame, None, None, undo_transform=False)
+        img_numpy[:, :, (2, 1, 0)]
+
+        return img_numpy
+
+
+    def detectObject(self, image):
+        with torch.no_grad():
+            return self.evalimage(image)
+
+
